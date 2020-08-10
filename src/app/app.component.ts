9bd3ef2997adfb697cfef2ca4408eae579d4ad9c
Akarsh-B <akarshbalu@gmail.com>
2020-08-10 00:54:11 -0500

    Added Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import {
+  faTwitter,
+  faFacebook,
+  faLinkedinIn,
+  faGithub,
+  faKaggle,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'MyPersonalWebsite';
+  faTwitter = faTwitter;
+  faFaceBook = faFacebook;
+  faLinkedIn = faLinkedinIn;
+  faGithub = faGithub;
+  faKaggle = faKaggle;
 }
