231fcbccfb566e1cbc173b7a117c0ac9561fea32
Akarsh-B <akarshbalu@gmail.com>
2020-12-05 12:36:52 -0600

    Angular Upgrade to v11

--- src/app/workexperience/workexperience.component.spec.ts
+++ src/app/workexperience/workexperience.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { WorkexperienceComponent } from './workexperience.component';
 
@@ -6,7 +6,7 @@ describe('WorkexperienceComponent', () => {
   let component: WorkexperienceComponent;
   let fixture: ComponentFixture<WorkexperienceComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ WorkexperienceComponent ]
     })
